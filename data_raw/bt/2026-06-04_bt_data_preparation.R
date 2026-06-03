b6658ff0a3eb267a8833f97445e81d76cb7065f6
Philippe Joly <philippe.joly@posteo.net>
2026-06-03 21:27:19 +0200

    change variable name

--- data_raw/bt/2026-06-04_bt_data_preparation.R
+++ data_raw/bt/2026-06-04_bt_data_preparation.R
@@ -22,10 +22,10 @@ leader <- c("J. Spahn",
 bt <- data.frame(party, seat_n, leader) |> 
   mutate(seat_share = seat_n / sum(seat_n),
          seat_share = round(seat_share, digits = 2),
-         gov = if_else(party %in% c("CDU/CSU", "SPD"),
+         role = if_else(party %in% c("CDU/CSU", "SPD"),
                        "Government",
                        "Opposition"),
-         gov = factor(gov, levels = c("Opposition", "Government"))) |> 
+         role = factor(role, levels = c("Opposition", "Government"))) |> 
   relocate(seat_share, .after = seat_n)
 
 saveRDS(bt, "data/2026-06-04_bt.rds")
